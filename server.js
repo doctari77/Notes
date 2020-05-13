a7b769fa707f78058127f617db485bab272cdbbb
Todd Leslie <spotontechnology@gmail.com>
2020-05-12 19:57:37 -0600

    notes move

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+const apiroutes = require ('./routes/apiroutes.js')
+const htmlroutes = require('./routes/htmlroutes.js')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use('/api', apiroutes)
+app.use('/', htmlroutes)
+
+
+app.listen(PORT, () => console.log (PORT))
